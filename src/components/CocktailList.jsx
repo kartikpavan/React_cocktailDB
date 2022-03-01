f2fdd27c1ebd814b291f84f718288e0bf568b94c
kartikpavan <kartik.varanasi@yahoo.com>
2022-03-01 20:57:54 +0530

    Setting up starting files

---
+++ src/components/CocktailList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Cocktail from './Cocktail';
+import Loading from './Loading';
+
+function CocktailList() {
+  return <div>CocktailList</div>;
+}
+
+export default CocktailList;
---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SearchForm() {
+  return (
+    <div>
+      <h2>Search Form Componenet</h2>
+    </div>
+  );
+}
+
+export default SearchForm;
